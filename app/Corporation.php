21104a999f72467477afd2429c06950b52c41341
Sabeto051 <bedoya.santi@gmail.com>
2020-04-18 12:47:36 -0500

    migraciones casi listas

--- app/Corporation.php
+++ app/Corporation.php
@@ -2,10 +2,43 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Corporation extends Model
+class Corporation extends Authenticatable
 {
+    use Notifiable;
+
+    protected $guard = 'corporation';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
     public function recyclers()
     {
         return $this->hasMany('App\Recycler');
