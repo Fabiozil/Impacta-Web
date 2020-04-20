67ed55119ad253cdc036be38716b355ea1fe1af8
sofia323 <sofiat323@hotmail.com>
2020-04-19 22:19:06 -0500

    Login
    lista vista login

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,10 @@ Route::get('/user', 'UserController@index');
 Route::get('/corporation', 'CorporationController@index');
 Route::get('/recycler', 'RecyclerController@index');
 Route::get('/notification', 'NotificationController@index');
+//despues cambiamos esto de abajo por el controlador que es
+Route::get('/registroCorp', function(){
+    return view('registroCorp');
+});
+
 
 
