c4eb505358a7577620b9a65abe4702e21b55fc12
julianalondono <juli1503londono@gmail.com>
2020-05-08 12:56:24 -0500

    detalles perfil

--- resources/views/Busqueda/resultsUser.blade.php
+++ resources/views/Busqueda/resultsUser.blade.php
@@ -44,10 +44,8 @@
                                         <div class="row">A 100km de ti</div>
                                         <div class="row">Vivienda particular</div> 
                                         <div class="row justify-content-end">
-                                            <a href="{{ url('/perfilu') }}">
-                                                <button type="submit" class="btn btn-primary" style="margin-bottom: 10px; margin-left: -15px">Ver perfil</button>
-                                            </a>
-                                            <a href="{{ url('/perfilc') }}">
+                                        <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#detallesModal" style="margin-bottom: 10px; margin-left: -15px">Ver perfil</button>      
+                                            <a href="{{ url('/') }}">
                                                 <button type="submit" class="btn btn-primary" style="margin-left: 10px; margin-bottom: 10px">Ver en mapa >></button>
                                             </a>
                                         </div>
@@ -74,10 +72,8 @@
                                         <div class="row">A 100km de ti</div>
                                         <div class="row">Unidad residencial</div> 
                                         <div class="row justify-content-end">
-                                            <a href="{{ url('/perfilu') }}">
-                                                <button type="submit" class="btn btn-primary" style="margin-bottom: 10px; margin-left: -15px">Ver perfil</button>
-                                            </a>
-                                            <a href="{{ url('/perfilc') }}">
+                                        <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#detallesModal" style="margin-bottom: 10px; margin-left: -15px">Ver perfil</button>      
+                                            <a href="{{ url('/') }}">
                                                 <button type="submit" class="btn btn-primary" style="margin-left: 10px; margin-bottom: 10px">Ver en mapa >></button>
                                             </a>
                                         </div>
@@ -98,10 +94,8 @@
                                         <div class="row">A 100km de ti</div>
                                         <div class="row">Vivienda particular</div> 
                                         <div class="row justify-content-end">
-                                            <a href="{{ url('/perfilu') }}">
-                                                <button type="submit" class="btn btn-primary" style="margin-bottom: 10px; margin-left: -15px">Ver perfil</button>
-                                            </a>
-                                            <a href="{{ url('/perfilc') }}">
+                                        <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#detallesModal" style="margin-bottom: 10px; margin-left: -15px">Ver perfil</button>      
+                                            <a href="{{ url('/') }}">
                                                 <button type="submit" class="btn btn-primary" style="margin-left: 10px; margin-bottom: 10px">Ver en mapa >></button>
                                             </a>
                                         </div>
@@ -128,10 +122,8 @@
                                         <div class="row">A 14km de ti</div>
                                         <div class="row">Unidad residencial</div> 
                                         <div class="row justify-content-end">
-                                            <a href="{{ url('/perfilu') }}">
-                                                <button type="submit" class="btn btn-primary" style="margin-bottom: 10px; margin-left: -15px">Ver perfil</button>
-                                            </a>
-                                            <a href="{{ url('/perfilc') }}">
+                                        <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#detallesModal" style="margin-bottom: 10px; margin-left: -15px">Ver perfil</button>      
+                                            <a href="{{ url('/') }}">
                                                 <button type="submit" class="btn btn-primary" style="margin-left: 10px; margin-bottom: 10px">Ver en mapa >></button>
                                             </a>
                                         </div>
@@ -152,10 +144,8 @@
                                         <div class="row">A 0km de ti</div>
                                         <div class="row">Vivienda particular</div> 
                                         <div class="row justify-content-end">
-                                            <a href="{{ url('/perfilu') }}">
-                                                <button type="submit" class="btn btn-primary" style="margin-bottom: 10px; margin-left: -15px">Ver perfil</button>
-                                            </a>
-                                            <a href="{{ url('/perfilc') }}">
+                                        <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#detallesModal" style="margin-bottom: 10px; margin-left: -15px">Ver perfil</button>      
+                                            <a href="{{ url('/') }}">
                                                 <button type="submit" class="btn btn-primary" style="margin-left: 10px; margin-bottom: 10px">Ver en mapa >></button>
                                             </a>
                                         </div>
@@ -167,10 +157,97 @@
                     </ul>
                 </div>
             </div>
+            <button type="submit" class="btn btn-primary" style="margin-bottom: 10px; margin-left: -15px">Regresar</button>
+        </div>
+        
+    </div>
+    
+
+    <div class="modal fade" id="detallesModal" tabindex="-1" role="dialog" aria-labelledby="favoritesModalLabel">
+    <div class="modal-dialog modal-lg" role="document" >
+        <div class="modal-content" >
+         <div class="modal-header">
+            <h4 class="modal-title" id="favoritesModalLabel">Datos del usuario</h4>
+             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>  
+        </div>
+        <div class="modal-body">
+
+        <div class="container">
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="row">
+                <div class="col-xl-4 col-lg-4 col-md-12 borde-derecha h-100">
+                    <div class="row h-100">
+                        <div class="container h-80 cont-rec">
+                            <div class="row">
+                                <div class="col-xl-12 h-100">
+                                    <div class="container">
+                                        <div class="py-5">    
+                                        </div>
+                                        <i class="fas fa-user user-icon center-img"></i>
+                                                                      
+                                        
+                                        <div class="py-4 text-center"> 
+                                        <h3>Dango123</h3>                   
+                                        <p>Se unió hace 2 meses</p> 
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-8 col-lg-8 col-md-12 borde-derecha h-100">
+                    <div class="row h-100">
+                        <div class="container cont-rec h-95">
+                            <div class="row h-100">
+                                <div class="col-xl-10">
+                                    <div class="py-5 subtitle">
+                                              
+                                    </div>
+                                    <br>
+                                                                
+                                    <div class="container cont-rec-nf m-0">
+                                        <form>
+                                            <div class="form-group w-100">
+                                            <div class="form-group row">
+                                                    <h3>Dirección </h3>
+                                                    <div class="col-sm-10">
+                                                    <h3>Carrera 24 No. 33 - 45</h3>
+                                                    </div>
+                                                </div>  
+                                                <div class="form-group row">
+                                                    <h3>Municipio</h3>
+                                                    <div class="col-sm-10">
+                                                    <h3>Medellin</h3>
+                                                    </div>
+                                                </div>  
+                                                <div class="form-group row">
+                                                    <h3>Reciclador asignado</h3>
+                                                    <div class="col-sm-10">
+                                                    <h3>Antonio Marín</h3>
+                                                    </div>
+                                                </div>              
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-
-
+        </div>
+      </div>
+      <div class="modal-footer"> 
+        <span class="pull-right"><button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button></span>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 @endsection
