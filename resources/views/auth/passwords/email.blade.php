d77fdae80a515e4163bde01bd50158fea058df80
Fabiozil <39010446+Fabiozil@users.noreply.github.com>
2020-05-10 22:49:44 -0500

    Mejora de las vistas

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -2,40 +2,60 @@
 
 @section('content')
 <div class=" d-flex justify-content-center align-content-center">
-<div id="Contenedor" class="col-4 container-fluid ">
-    <div class="Icon">
-              <span class="glyphicon glyphicon-exclamation-sign"></span>
+    <div id="Contenedor" class="col-4 container-fluid ">
+        <div class="card shadow">
+            <div class="card-header">
+                <div class="py-3 text-success text-center">
+                    <h2>Reestablecer contraseña</h2>
+                </div>
             </div>
-<div class="ContentForm">
-        <form action="{{ route('password.email') }}" method="post" name="FormEntrar">
-        @csrf
+            <div class="card-body">
+                <div class="Icon">
+                    <span class="glyphicon glyphicon-exclamation-sign"></span>
+                </div>
+                <div class="ContentForm">
+                    <p class="lead">Ingresa el correo electrónico con el que se había registrado la corporación para
+                        reestablecer la contraseña</p>
+                    <form action="{{ route('password.email') }}" method="post" name="FormEntrar">
+                        @csrf
+                        <div class="input-group input-group-lg">
+                            <span class="input-group-addon" id="sizing-addon1"><i
+                                    class="glyphicon glyphicon-list"></i></span>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                aria-describedby="sizing-addon1" placeholder="{{ __('E-Mail Address') }}" name="email"
+                                value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
 
+                        <br>
+                        <div class=" d-flex justify-content-center align-content-center">
+                            <button class="btn btn-lg btn-success btn-block btn-signin col-8" id="IngresoLog"
+                                type="submit">
+                                {{ __('Send Password Reset Link') }}
 
-            <div class="input-group input-group-lg">
-             <span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-list"></i></span>
-             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" aria-describedby="sizing-addon1" placeholder="{{ __('E-Mail Address') }}" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-             @error('email')
-                               <span class="invalid-feedback" role="alert">
-                                   <strong>{{ $message }}</strong>
-                               </span>
-                           @enderror
-           </div>
+                            </button>
+                        </div>
 
-           <br>
-           <div class=" d-flex justify-content-center align-content-center">
-           <button class="btn btn-lg btn-primary btn-block btn-signin col-8" id="IngresoLog" type="submit" >
-           {{ __('Send Password Reset Link') }}
+                    </form>
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+                </div>
+            </div>
+            <div class="card-footer">
+                <small id="emailHelp" class="form-text text-muted">
+                    Al hacer click en "enviar enlace" autorizas a enviar un correo a la direccion de correo electrónico de la corporacion. 
+                    En el correo se encontrará el instructivo para reestablecer la contraseña.
+                </small>
+            </div>
+        </div>
 
-           </button>
-           </div>
-
-       </form>
-       @if (session('status'))
-                   <div class="alert alert-success" role="alert">
-                       {{ session('status') }}
-                   </div>
-               @endif
     </div>
-    </div>
-    </div>
+</div>
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -4,59 +4,56 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+            <div class="card shadow">
+                <div class="card-header text-center text-success">
+                    <div class="py-3">
+                        <h2>{{ __('Reset Password') }}</h2>                        
+                    </div>
 
+                </div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
                         @csrf
-
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        {{-- <input type="hidden" name="token" value="{{ $token }}"> --}}
+                        <div class="form-group mx-auto w-75">
+                            <label for="email">{{ __('E-Mail Address') }}</label>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="form-group mx-auto w-75">
+                            <label for="password">{{ __('Password') }}</label>
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                            <small id="emailHelp" class="form-text text-muted">
+                                Ingresa la nueva contraseña
+                            </small>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
+                        <div class="form-group mx-auto w-75">
+                            <label for="password-confirm">{{ __('Confirm Password') }}</label>
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
+                        <div class="form-group row mx-auto w-75">
+                            <button type="submit" class="btn btn-success mx-auto w-75   ">
+                                {{ __('Reset Password') }}
+                            </button>
                         </div>
                     </form>
+                </div>
+                <div class="card-footer">
+                    <small id="emailHelp" class="form-text text-muted">
+                        Por favor verifica que la dirección de correo auto completada corresponda con la de tu corporación.
+                        Al hacer click en "reestablecer contraseña" autorizas a reemplazar la contraseña actual con la ingresada.
+                    </small>
                 </div>
             </div>
         </div>
